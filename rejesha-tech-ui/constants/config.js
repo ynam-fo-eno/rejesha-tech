3730890ede4e6307fedb14c3a908e6bd40c75aac
ynam-fo-eno <1202dtim@gmail.com>
2026-04-23 23:20:56 +0300

    Always has to be a fucking joke this Wi-Fi, huh?

--- rejesha-tech-ui/constants/config.js
+++ rejesha-tech-ui/constants/config.js
@@ -3,6 +3,6 @@ import { Platform } from 'react-native';
 // This will now look at the Expo Dashboard first, then fall back to local if empty
 export const BASE_URL = process.env.EXPO_PUBLIC_API_URL 
   ? process.env.EXPO_PUBLIC_API_URL 
-  : (Platform.OS === 'web' ? 'http://localhost:3000' : 'http://192.168.100.45:3000');
+  : (Platform.OS === 'web' ? 'http://localhost:3000' : 'http://10.206.76.60:3000');
 
 console.log("Connecting to:", BASE_URL); // Helps you debug on your phone later
