c029320fe005e4204b37a095269f731d89122531
ynam-fo-eno <1202dtim@gmail.com>
2026-04-23 22:22:12 +0300

    How many times shall we try with the bloody APK

--- rejesha-tech-api/server.js
+++ rejesha-tech-api/server.js
@@ -30,6 +30,6 @@ app.get('/', (req, res) => {
 // 4. START SERVER
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-  console.log(`🔗 Local Address: http://localhost:${PORT}`);
+  console.log(` Server running on port ${PORT}`);
+  console.log(` Local Address: http://localhost:${PORT}`);
 });
